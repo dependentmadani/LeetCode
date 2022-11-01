bbb8cdfa063c5ee0672d86309e93e30d531f9fab
hodormad <47648515+dependentmadani@users.noreply.github.com>
2022-11-01 02:15:41 +0100

    Time: 2471 ms (30.38%), Space: 13.8 MB (90.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,33 @@
+
 class Solution:
+
+    
     def longestPalindrome(self, s: str) -> str:
         LongPal = ""
         len_longPal = 0
         
         for i in range(len(s)):
             left , right = i, i
-            while (left >= 0 and right < len(s) and s[left] == s[right]):
-                if (right - left + 1 > len_longPal):
-                    LongPal = s[left: right + 1]
-                    len_longPal = right - left + 1
-                left -= 1
-                right += 1
+            new = ""
+            new, left, right,len_longPal = self.check_palindrom(s, left, right,len_longPal)
+            if (new != ""):
+                LongPal = new
             
             left, right = i, i + 1
-            while (left >= 0 and right < len(s) and s[left] == s[right]):
+            new = ""
+            new, left, right,len_longPal = self.check_palindrom(s, left, right,len_longPal)
+            if (new != ""):
+                LongPal = new
+        
+        return LongPal
+    
+    def check_palindrom(self, s:str, left:int, right:int, len_longPal:int):
+        LongPal = ""
+        
+        while (left >= 0 and right < len(s) and s[left] == s[right]):
                 if (right - left + 1 > len_longPal):
                     LongPal = s[left: right + 1]
                     len_longPal = right - left + 1
                 left -= 1
                 right += 1
-        
-        return LongPal
+        return LongPal, left, right, len_longPal
